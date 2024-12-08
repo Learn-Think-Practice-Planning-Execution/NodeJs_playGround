180850cce6d1649f90cb26cf2f4377cc3a532e6a
vikas phougat <vikas.phougat@kalolytic.com>
2024-12-08 16:06:39 +0530

    till https module

--- moduleWiseStudy/http_module_5.js
+++ moduleWiseStudy/http_module_5.js
@@ -1,4 +1,10 @@
 import http from 'http';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import fsPromise from 'fs/promises';
+import path from 'path';
+
+const currentdir = path.dirname(fileURLToPath(import.meta.url));
 
 // function
 const requestListener = (req, res) => {
@@ -20,18 +26,65 @@ const requestListener = (req, res) => {
 const server = http.createServer((req, res) => {
   console.log('req', req.url, req.method, req.headers);
 
+  const method = req.method;
+
   if (req.url == '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>this is my first page</title></head>');
-    res.write('<body> <h1>this is body</h1></body');
+    res.write(
+      '<body> <h1>this is body</h1><br><form action="/contactus" method="POST"><input name="message" type="text" ><br> <button type="submit">send request</button></form></body'
+    );
     res.write('</html>');
     return res.end();
-  } else if (req.url == '/about') {
+  } else if (req.url == '/about' && method === 'GET') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>this is my first page</title></head>');
     res.write('<body> <h1>this is body about page</h1></body');
+    res.write('</html>');
+    res.end();
+  } else if (req.url == '/contactus' && method === 'POST') {
+    // parsing request body
+    const body_data_parsing = [];
+    req.on('data', (chunks) => {
+      console.log('chunks', chunks);
+      body_data_parsing.push(chunks);
+    });
+
+    return req.on('end', () => {
+      const parsedbody = Buffer.concat(body_data_parsing).toString();
+      console.log('buffer', parsedbody);
+      const messageValue = parsedbody.split('=')[1];
+      const filePath = path.join(currentdir, 'contactus.txt');
+      //   fs.writeFileSync(filePath, messageValue, 'utf-8'); // block the next line execution  ,, use sync when needed
+      fs.writeFile(filePath, messageValue, 'utf-8', (error) => {
+        console.log('error', error);
+        if (error) {
+          console.log('error', error);
+          req.statusCode = 400;
+          req.write({
+            error
+          });
+        } else {
+          res.statusCode = 200;
+          res.setHeader('Location', '/');
+          res.end();
+        }
+      }); // block the next line execution  ,, use sync when needed
+
+      // according to dependances , move below code excution order
+      //   req.statusCode = 200;
+      //   req.setHeader('Location', '/');
+      //   res.end();
+    });
+    // req.statusCode = 200;
+    // req.setHeader('Location', '/');
+    //  res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>this is my last page</title></head>');
+    res.write('<body> <h1>this is about us page  about page</h1></body');
     res.write('</html>');
     res.end();
   }
@@ -41,4 +94,7 @@ const server = http.createServer((req, res) => {
   //   process.exit(); // it will end the process with a status  code // stop the node server
 });
 
-server.listen(4000);
+const port = 4000;
+server.listen(port, () => {
+  console.log('listening on ', port);
+});
--- moduleWiseStudy/task/task_1.js
+++ moduleWiseStudy/task/task_1.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import fs from 'fs';
 import fsPromises from 'fs/promises';
 import EventEmitter from 'events';
+import { randomUUID } from 'crypto';
 
 const emitter = new EventEmitter();
 // current directory
@@ -12,135 +13,188 @@ const userdetailsjson = 'userdetailsjson.json';
 const currentfilepath = path.join(currentdir, userdetailsjson);
 // const currentfilepath = path.join(currentdir, 'eventemittercounter');
 
-const eventCounter = {
-  user_login_count: 0,
-  user_logout_count: 0,
-  user_purchase_count: 0,
-  user_profile_update_count: 0,
-  total_event_emit_count: 0
+let UserData = {
+  eventCounter: {
+    user_login_count: 0,
+    user_logout_count: 0,
+    user_purchase_count: 0,
+    user_profile_update_count: 0,
+    total_event_emit_count: 0
+  }
 };
-let newUser = {};
-let totalData = [];
-const userDetailsQuery = async (type, collectuserdata) => {
-  // if new user then entry a newuser and update the file
 
-  if (type === 'login') {
-    newUser = {
-      ...newUser,
-      ...collectuserdata
-    };
-    eventCounter.user_login_count++; //      // eventCounter.user_login_count += 1;
-  } else if (type === 'logout') {
-    newUser = {
-      ...newUser,
-      ...collectuserdata
-    };
-    eventCounter.user_logout_count += 1;
-  } else if (type === 'purchase') {
-    newUser = {
-      ...newUser,
-      ...collectuserdata
-    };
-    eventCounter.user_purchase_count++; //   eventCounter.user_purchase_count += 1;
-  } else if (type === 'profile_update') {
-    newUser = {
-      ...newUser,
-      ...collectuserdata
-    };
-    eventCounter.user_profile_update_count++; //   eventCounter.user_profile_update_count += 1;
-  } else {
-    console.log('none');
+// function for get data from file and update a user details
+
+// write data to the file and save it
+
+const getDatafromJson = async () => {
+  let jsArryObj = [];
+  try {
+    // Check if the file exists
+    await fsPromises.access(currentfilepath);
+    const readingData = await fsPromises.readFile(currentfilepath, 'utf8');
+
+    // parse the data
+    jsArryObj = readingData ? JSON.parse(readingData) : [];
+    console.log('jsArryObj', jsArryObj);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('File does not exist. Initializing an empty file.');
+      // Initialize an empty JSON file
+      await fsPromises.writeFile(currentfilepath, '[]', 'utf8');
+    } else {
+      console.error('Error accessing file:', error);
+    }
   }
 
-  // total count
-  const totalcount = Object.entries(eventCounter).reduce(
-    (perValue, curValue, CurINdex, arrOfStrings) => {
-      //   console.log(
-      //     'perValue, curValue, CurINdex, arrOfStrings',
-      //     perValue,
-      //     curValue,
-      //     CurINdex,
-      //     arrOfStrings
-      //   );
-      console.log('outer', curValue[0] !== 'total_event_emit_count');
-      if (curValue[0] !== 'total_event_emit_count') {
-        console.log('inter', curValue[1]);
-        return perValue + curValue[1];
-      }
+  return jsArryObj;
+};
 
-      return perValue;
-    },
-    0
-  );
-  console.log('totalcount', totalcount);
-  eventCounter.total_event_emit_count = totalcount;
+const writeDataToFile = async (datatowrite) => {
   // add data to a file
-
-  const newUserAllData = {
-    ...newUser,
-    ...eventCounter
-  };
-
+  console.log('datatowrite', datatowrite);
   try {
     // Convert JavaScript object to JSON string
-    const jsonData = JSON.stringify(newUserAllData, null, 2);
+    const jsonData = JSON.stringify(datatowrite);
+    // const jsonData = JSON.stringify(datatowrite, null, 2);
     const data = await fsPromises.writeFile(currentfilepath, jsonData, 'utf-8');
+    return data;
   } catch (error) {
     console.error(error);
+    return error;
   }
+
+  //   return data;
 };
 
 // save all data to a file async and
 // append all data to that  file
 
-const getDatafromJson = async () => {
-  let readingData = '';
-  let jsobj = '';
+const userLoginFunHandler = async (loginData) => {
+  let totalData = [];
   try {
-    // Check if the file exists
-    await fsPromises.access(currentfilepath);
+    const dataJson = await getDatafromJson();
 
-    readingData = await fsPromises.readFile(currentfilepath, 'utf8');
-    // parse the data
+    // update
 
-    jsobj = JSON.parse(readingData);
-    // check if the user match pervious user then ,, update only that data
-    // if (jsobj)
-    //   // get particular user data if avaliable then update state if not then new entry
-
-    console.log('jsobj', jsobj);
+    // if user exist update current object in array
+    if (dataJson.filter((item) => item.username === loginData.username)[0]) {
+      //   let PrevData = dataJson.filter((item) => item.username === loginData.username)[0];
+      totalData = dataJson.map((item) => {
+        if (item.username === loginData.username) {
+          return {
+            ...item,
+            ...loginData,
+            login: true,
+            eventCounter: {
+              ...item.eventCounter,
+              user_login_count: item.eventCounter.user_login_count + 1
+            }
+          };
+        }
+        return item;
+      });
+      //   console.log('dataJson PrevData', PrevData);
+      // update current UserData and counter data
+      console.log('dataJson totalData', totalData);
+      //   totalData = JSON.parse(JSON.stringify(dataJson));
+      await writeDataToFile(totalData);
+    } else {
+      // if not matched then add a new user
+      dataJson.push({
+        // ...UserData,
+        ...loginData,
+        user_id: randomUUID(),
+        login: true,
+        eventCounter: {
+          user_login_count: 1
+          // Add more event counters as needed
+        }
+      });
+      totalData = dataJson;
+      await writeDataToFile(totalData);
+    }
+    // totalData = PrevData;
+    return totalData;
   } catch (error) {
-    console.error(error);
+    console.log('error', error);
   }
-
-  return readingData ? jsobj : [];
+  return totalData;
 };
-emitter.on('login', async (data) => {
-  console.log('user loged in', data);
 
-  const dataJson = await getDatafromJson();
-  // check if file exist
-  totalData = dataJson;
-  console.log('user data ', totalData, dataJson);
+///////////////////////////////////////////////////////////////////////////////////////
+emitter.on('login', (data) => {
+  //   console.log('user loged in', data);
+  userLoginFunHandler(data);
   //   userDetailsQuery('login', data);
 });
-emitter.on('logout', (data) => {
-  console.log('user logout');
-  userDetailsQuery('logout', data);
-});
-emitter.on('purchase', (data) => {
-  console.log('user purchase');
-  userDetailsQuery('purchase', data);
-});
-emitter.on('profile_update', (data) => {
-  console.log('user profile_update');
-  userDetailsQuery('profile_update', data);
-});
+// emitter.on('logout', (data) => {
+//   console.log('user logout');
 
-// emitter.emit('login', { name: 'vikas', login: true, user_logout: false });
-// emitter.emit('purchase', { user_purchase: 'protien' });
+//   //   userDetailsQuery('logout', data);
+// });
+// emitter.on('purchase', (data) => {
+//   console.log('user purchase');
+//   userDetailsQuery('purchase', data);
+// });
+// emitter.on('profile_update', (data) => {
+//   console.log('user profile_update');
+//   userDetailsQuery('profile_update', data);
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+emitter.emit('login', { username: 'vikas', user_password: 'something_new3122' });
+emitter.emit('login', { username: 'viki', user_password: 'something' });
+
+// emitter.emit('purchase', { username: 'vikas', user_ids: '', user_purchase: 'protien' });
 // emitter.emit('profile_update', { profile_update: true });
 // emitter.emit('logout', { name: 'vikas', user_logout: true, login: false });
 
-console.log('eventCounter', eventCounter);
-console.log('newUser', newUser);
+// const userDetailsQuery = async (type, collectuserdata) => {
+//     // if new user then entry a UserData and update the file
+//     if (type === 'logout') {
+//       UserData = {
+//         ...UserData,
+//         ...collectuserdata
+//       };
+//       UserData.eventCounter.user_logout_count += 1;
+//     } else if (type === 'purchase') {
+//       UserData = {
+//         ...UserData,
+//         ...collectuserdata
+//       };
+//       UserData.eventCounter.user_purchase_count++; //   eventCounter.user_purchase_count += 1;
+//     } else if (type === 'profile_update') {
+//       UserData = {
+//         ...UserData,
+//         ...collectuserdata
+//       };
+//       UserData.eventCounter.user_profile_update_count++; //   eventCounter.user_profile_update_count += 1;
+//     } else {
+//       console.log('none');
+//     }
+
+//     // total count
+//     const totalcount = Object.entries(UserData.eventCounter).reduce(
+//       (perValue, curValue, CurINdex, arrOfStrings) => {
+//         //   console.log(
+//         //     'perValue, curValue, CurINdex, arrOfStrings',
+//         //     perValue,
+//         //     curValue,
+//         //     CurINdex,
+//         //     arrOfStrings
+//         //   );
+//         console.log('outer', curValue[0] !== 'total_event_emit_count');
+//         if (curValue[0] !== 'total_event_emit_count') {
+//           console.log('inter', curValue[1]);
+//           return perValue + curValue[1];
+//         }
+
+//         return perValue;
+//       },
+//       0
+//     );
+//     console.log('totalcount', totalcount);
+//     UserData.eventCounter.total_event_emit_count = totalcount;
+//   };
